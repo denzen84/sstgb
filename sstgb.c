3a717dc5e0f5022a0e17f67bbff119d99439acdc
denzen84 <dentall@mail.ru>
2019-01-05 20:59:34 +0300

    added native lubcurl proxy support

--- sstgb.c
+++ sstgb.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
@@ -21,6 +20,7 @@ struct {
     char *comment;
     char *path;
     char *text;
+    char *proxy_addr;
     int isRemove;
     int isWeakConfig;
     int useFileConfig;
@@ -30,7 +30,7 @@ struct {
 void printHelp(void) {
 printf(
 		"-----------------------------------------------------------------------------\n"
-		"| sstgb: simply sender telegram bot                                      v1.1|\n"
+		"| sstgb: simply sender telegram bot                                      v1.2|\n"
 		"-----------------------------------------------------------------------------\n"
 		"\n"
 		"This telegram bot sends text and pictures according to command line paramaters\n"
@@ -49,14 +49,15 @@ printf(
 		"--userid <id>            User ID\n"
 		"--fileconfigs            Read bot token and userid from files .token and .userid accordingly\n"
 		"--path                   Working path for --fileconfigs option (last slash required!)\n"
-		"--comment                Comment for picture/audio/video/document\n"
+                "--proxy                  Use libcurl proxy. Examples: socks5://addr.org:8564 or http://addr.org:8564\n"
+		"--comment                Comment for picture/audio/video\n"
 		"--remove                 Remove(!) file after use for --sendpic, --sendvideo, --sendaudio, --senddoc\n"
 		"--weakconfig             Simplified command line parameters pre-check for debug\n"
 		"\n"
 		"To use --fileconfigs there are should be two files:\n"
 		"1) .token  - text file where first string is telegram bot token\n"
 		"2) .userid - text file where first string is user ID\n"
-		"\n"		
+		"\n"
 );
 
 }
@@ -80,6 +81,7 @@ void printConfig(void) {
 			"Use file config.........%d\n"
 			"Working path............%s\n"
 			"Use weak config.........%d\n"
+			"Proxy...................%s\n"
 			"\n",
 			sstgbconf.token,
 			sstgbconf.user_id,
@@ -92,28 +94,29 @@ void printConfig(void) {
 			sstgbconf.isRemove,
 			sstgbconf.useFileConfig,
 			sstgbconf.path,
-			sstgbconf.isWeakConfig
+			sstgbconf.isWeakConfig,
+			sstgbconf.proxy_addr
 );
 }
 
 int readFileConfig(void) {
-	
+
 	char *tokenfilename, *useridfilename;
-	
+
 	if (sstgbconf.path == NULL) {
 		tokenfilename = strdup(FILENAME_TOKEN);
 		useridfilename = strdup(FILENAME_USERID);
-		
+
 	} else {
 		tokenfilename = (char*)calloc(strlen(sstgbconf.path) + sizeof(FILENAME_TOKEN) + 1, sizeof(char));
 		strcpy(tokenfilename, sstgbconf.path);
 		strcat(tokenfilename, FILENAME_TOKEN);
-		
+
 		useridfilename = (char*)calloc(strlen(sstgbconf.path) + sizeof(FILENAME_USERID) + 1, sizeof(char));
 		strcpy(useridfilename, sstgbconf.path);
 		strcat(useridfilename, FILENAME_USERID);
 	}
-	
+
 	FILE *fp = fopen(tokenfilename, "r");
 	if (fp == NULL) {
 		printf("Failed to open .token file\n");
@@ -133,7 +136,7 @@ int readFileConfig(void) {
 		printf("Failed to open .userid file\n");
 	    return -1;
 	}
-	
+
 	if (fscanf(fp, "%lld", &sstgbconf.user_id) == 0) {
 		printf("Reading user ID failed\n");
 		fclose(fp);
@@ -207,6 +210,9 @@ int main(int argc, char *argv[])
 		else if (!strcmp(argv[j],"--comment") && more) {
 					sstgbconf.comment = strdup(argv[++j]);
 		}
+		else if (!strcmp(argv[j],"--proxy") && more) {
+					sstgbconf.proxy_addr = strdup(argv[++j]);
+		}
 		else if (!strcmp(argv[j],"--help")) {
 			printHelp();
 			return 0;
@@ -218,20 +224,20 @@ int main(int argc, char *argv[])
 			return -1;
 		}
 	}
-	
+
 	if (sstgbconf.useFileConfig) {
 		if (readFileConfig() != 0) {
 			printf("Failed to read config files. Checking command line config...\n");
 		}
 	}
-	
+
 	if(checkConfig() != 0) {
 		printHelp();
 		printConfig();
 		printf("Configuration error. Nothing to do.\n\n");
 		return -1;
 	}
-	
+
 
     telebot_handler_t handle;
     if (telebot_create(&handle, sstgbconf.token) != TELEBOT_ERROR_NONE) {
@@ -243,6 +249,13 @@ int main(int argc, char *argv[])
     // Main work routine
     
     telebot_error_e ret;
+
+    if (sstgbconf.proxy_addr != NULL) {
+    	ret = telebot_use_proxy(handle, sstgbconf.proxy_addr);
+    	if (ret != TELEBOT_ERROR_NONE) {
+    		printf("Failed to init proxy: %d \n", ret);
+    	}
+    }
 
     if (sstgbconf.text != NULL) {
     	ret = telebot_send_message(handle, sstgbconf.user_id, sstgbconf.text, "", false, false, 0, "");
@@ -293,6 +306,9 @@ int main(int argc, char *argv[])
 	free(sstgbconf.docfile);
 	free(sstgbconf.path);
 	free(sstgbconf.comment);
+	free(sstgbconf.proxy_addr);
+
 	
     return 0;
 }
+
